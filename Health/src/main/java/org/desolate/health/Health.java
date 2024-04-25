5ca33100a357f8a80e65af4b63a3d826fe75963d
Do0oMores <ws2004421@outlook.com>
2024-04-26 01:26:36 +0800

    增加切换世界同步血量

--- Health/src/main/java/org/desolate/health/Health.java
+++ Health/src/main/java/org/desolate/health/Health.java
@@ -2,10 +2,12 @@ package org.desolate.health;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.World;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -90,4 +92,34 @@ public final class Health extends JavaPlugin implements Listener {
             e.printStackTrace();
         }
     }
+
+    //玩家切换世界
+    @EventHandler
+    public void onPlayerChangeWorld(PlayerChangedWorldEvent event){
+        Player player= event.getPlayer();
+        World world= event.getFrom();
+        double health = config.getDouble("最大玩家血量");
+        if (config.contains("最大玩家血量")) {
+            if (health > 0.0) {
+                player.setMaxHealth(health);
+            } else {
+                config.set("最大玩家血量", 60.0);
+                player.setMaxHealth(60);// 设置最大血量为60
+                player.sendMessage(ChatColor.RED + "血量需为正数，已将血量重置为60");
+            }
+        }
+        // 从配置文件读取玩家血量
+        if (config.contains(player.getUniqueId().toString())) {
+            double NowHealth = config.getDouble(player.getUniqueId().toString());
+            //增加判断，如果配置中保存玩家血量大于最大血量
+            //则直接设置玩家血量为最大血量，否则会出现血量不匹配报错
+            if (NowHealth > health) {
+                player.setHealth(health);
+                //重置超出阈值的玩家血量
+                player.sendMessage(ChatColor.RED + "你的血量已超出阈值，现已重置");
+            } else
+                player.setHealth(NowHealth);
+        }
+    }
+
 }
